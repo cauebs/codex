c849de3b6a793360232d41d704e7b667f438c5e0
Cauê Baasch de Souza <cauebs@protonmail.com>
2017-12-21 17:22:56 -0200

    Fixed tags in setup.py

--- setup.py
+++ setup.py
@@ -25,12 +25,12 @@ setup(
     },
 
     classifiers=[
-        'Environment:: Console',
-        'License:: OSI Approved:: MIT License',
-        'Programming Language:: Python',
-        'Programming Language:: Python:: 3',
-        'Programming Language:: Python:: 3.6',
-        'Programming Language:: Python:: 3.7',
-        'Programming Language:: Python:: 3:: Only',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
     ],
 )
